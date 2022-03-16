0e4d2ae60b44cbd13e10534175b8996239d8ba48
Micaella Morales <morales.micaella@gmail.com>
2022-03-15 20:31:58 -0700

    Initial Commit

---
+++ Honeymoon/App/HoneymoonApp.swift
@@ -0,0 +1,17 @@
+//
+//  HoneymoonApp.swift
+//  Honeymoon
+//
+//  Created by Micaella Morales on 3/15/22.
+//
+
+import SwiftUI
+
+@main
+struct HoneymoonApp: App {
+  var body: some Scene {
+    WindowGroup {
+      ContentView()
+    }
+  }
+}
